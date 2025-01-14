7057f8394cfee7d51125e42a13515622a45d9ebb
silver <slvrsrfr.28@gmail.com>
2025-01-14 13:30:15 -0700

    name + number updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "Silver Castillo",
+    name: "SILVER",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
         email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        phone: "+17802620417",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
